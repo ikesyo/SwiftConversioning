b3d60aaa96258d1c158cc890d43550019f9bc981
Syo Ikeda <suicaicoca@gmail.com>
2014-06-13 16:37:48 +0900

    Initial commit

---
+++ SwiftConversioning/SwiftConversioning.h
@@ -0,0 +1,19 @@
+//
+//  SwiftConversioning.h
+//  SwiftConversioning
+//
+//  Created by Syo Ikeda on 6/13/14.
+//  Copyright (c) 2014 Syo Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftConversioning.
+FOUNDATION_EXPORT double SwiftConversioningVersionNumber;
+
+//! Project version string for SwiftConversioning.
+FOUNDATION_EXPORT const unsigned char SwiftConversioningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftConversioning/PublicHeader.h>
+
+
