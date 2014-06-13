3d41888d3105e15a41025455428604a365d8000c
Syo Ikeda <suicaicoca@gmail.com>
2014-06-13 16:43:28 +0900

    Add String -> NSURL, NSURLRequest conversion

---
+++ SwiftConversioning/StringConversionExt.swift
@@ -0,0 +1,19 @@
+//
+//  StringConversionExt.swift
+//  SwiftConversioning
+//
+//  Created by Syo Ikeda on 6/13/14.
+//  Copyright (c) 2014 Syo Ikeda. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    @conversion func __conversion() -> NSURL {
+        return NSURL(string: self)
+    }
+
+    @conversion func __conversion() -> NSURLRequest {
+        return NSURLRequest(URL: self)
+    }
+}
